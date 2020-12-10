4f77402ec2065d2e4d0c78b56a2ea98958f72a7c
jiwonjung <4dakto@gmail.com>
2020-12-11 00:12:16 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { gql, useLazyQuery, useQuery, useReactiveVar } from "@apollo/client";
+import { sortOrder } from "./index";
 
-function App() {
+const COUNTRY = gql`
+  query Country($code: ID!) {
+    country(code: $code) {
+      code
+      name
+      emoji
+      nameWithEmoji @client
+    }
+  }
+`;
+
+const COUNTRIES = gql`
+  query Countires {
+    testList @client {
+      cnt
+    }
+    sortedCountries @client {
+      code
+      name
+      emoji
+      nameWithEmoji
+    }
+    countries {
+      code
+      name
+      emoji
+      nameWithEmoji @client
+    }
+  }
+`;
+
+const App = () => {
+  const direction = sortOrder();
+  const [code, setCode] = useState("");
+  // const { data: countryData, loading: countryLoading, error } = useQuery(
+  //   COUNTRY,
+  //   {
+  //     variables: { code },
+  //     skip: code.length !== 2,
+  //   }
+  // );
+
+  // const { data, loading } = useQuery(COUNTRIES); // 왜 안될까?
+  const [exec, { data, loading }] = useLazyQuery(COUNTRIES);
+  useEffect(() => {
+    exec();
+  }, [exec]);
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("sortOrder:ASC");
+      sortOrder("ASC");
+    }, 2000);
+  }, []);
+
+  console.log("countries data", data);
+  // const handleChange = (e) => {
+  //   setCode(e.target.value);
+  // };
+  console.log("data.testList", data?.testList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*{error && <h1>You broke it! {error.message}</h1>}*/}
+      {/*<input type="text" value={code} onChange={handleChange} />*/}
+      {/*{!countryData || countryLoading ? (*/}
+      {/*  <h1>loading country...</h1>*/}
+      {/*) : (*/}
+      {/*  <>*/}
+      {/*    <h1>Country</h1>*/}
+      {/*    <h2>{countryData.country.nameWithEmoji}</h2>*/}
+      {/*  </>*/}
+      {/*)}*/}
+
+      {!data || loading ? (
+        <h1>Loading countries...</h1>
+      ) : (
+        <>
+          <h1>Countires with Flags</h1>
+          {/*<ul>*/}
+          {/*  {data.sortedCountries.map((country, idx) => {*/}
+          {/*    return (*/}
+          {/*      <li key={idx}>*/}
+          {/*        <h2>{country.nameWithEmoji}</h2>*/}
+          {/*      </li>*/}
+          {/*    );*/}
+          {/*  })}*/}
+          {/*</ul>*/}
+          {data?.testList?.[0].cnt}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          testList: (existing, { readField }) => {
+            const direction = sortOrder();
+            if (direction === "DESC") {
+              return [{ cnt: 1 }];
+            } else {
+              return [{ cnt: 2 }];
+            }
+          },
+          sortedCountries: (existing, { readField }) => {
+            const direction = sortOrder();
+            const countires = [...(readField("countries") ?? [])];
+
+            return countires.sort((a, b) => {
+              const aName = readField("name", a);
+              const bName = readField("name", b);
+
+              if (direction === "DESC") {
+                if (aName < bName) return 1;
+                if (aName > bName) return -1;
+                return 0;
+              } else {
+                return b - a;
+                if (aName < bName) return -1;
+                if (aName > bName) return 1;
+                return 0;
+              }
+            });
+          },
+          country: {
+            read: (existing, { toReference, args }) => {
+              const countryRef = toReference({
+                __typename: "Country",
+                code: args.code,
+              });
+              return existing ?? countryRef;
+            },
+          },
+        },
+      },
+      Country: {
+        keyFields: ["code"],
+        fields: {
+          nameWithEmoji: {
+            read: (_, { readField }) => {
+              const name = readField("name");
+              const emoji = readField("emoji");
+
+              return `${name} ${emoji}`;
+            },
+          },
+        },
+      },
+    },
+  }),
+});
+
+export const sortOrder = makeVar("DESC");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
